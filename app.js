d7d44cb9357c799ab438724e8d44ee8624bae7b8
Sushil Kumar (Trianz) <Sushil.Kumar@trianz.com>
2018-07-18 15:22:40 +0530

    added comments

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
+// add comments
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
